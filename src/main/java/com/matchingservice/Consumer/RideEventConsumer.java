c4e32e5710066f26216391bfded92370bda6eeec
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-12 20:26:15 +0545

    feat(Ride Event Consumer)implemented for kafka

---
+++ src/main/java/com/matchingservice/Consumer/RideEventConsumer.java
@@ -0,0 +1,34 @@
+package com.matchingservice.Consumer;
+
+
+import com.matchingservice.Service.MatchingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class RideEventConsumer {
+
+    private final MatchingService matchingService;
+
+    @KafkaListener(topics = "ride-events")
+    public void consume(RideEvent event) {
+
+        log.info("RideEvent received: {}", event);
+
+        switch (event.getType()) {
+
+            case RIDE_REQUESTED ->
+                    matchingService.matchNewRide(event.getRideId());
+
+            case DRIVER_CANCELLED ->
+                    matchingService.reassignRide(event.getRideId());
+
+            default -> {
+                // ignore
+            }
+        }
+    }
+}
