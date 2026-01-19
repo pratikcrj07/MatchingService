22cb30e08858347d6e9c0b26e69bbaca0872589a
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-19 23:55:35 +0545

    feat(location)tried to implement geo coding service

--- src/main/java/com/matchingservice/Service/MatchingService.java
+++ src/main/java/com/matchingservice/Service/MatchingService.java
@@ -1,16 +1,26 @@
 package com.matchingservice.Service;
 
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Slf4j
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class MatchingService {
 
-    public void reassignRide(Long rideId) {
-        log.info("Starting reassignment flow for rideId={}", rideId);
+    private final GeocodingService geocodingService;
 
+    public void matchNewRide(Long rideId) {
+        // Example: convert passenger address to coordinates
+        Optional<double[]> coords = geocodingService.getCoordinates("Kathmandu, Nepal");
+        coords.ifPresent(c -> {
+            double lat = c[0];
+            double lon = c[1];
+            System.out.println("Passenger coordinates: " + lat + ", " + lon);
 
+            // TODO: Use Redis geospatial to find nearby drivers
+        });
     }
 }
